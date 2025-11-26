5df23b0cc3365f7b0ed2ba4efc4323456d2a2da1
NATHAN061 <nathanjunior061@gmail.com>
2025-11-26 14:15:28 +0000

    index.css

---
+++ script.js
@@ -0,0 +1,21 @@
+function validaFora() {  
+cnst email = document.getElementByid('email');
+let emailtext = email.value.tria();
+let valido = false;
+if (emailtext.includes("@"));
+}
+else { 
+valido = false;
+
+
+
+}
+
+let partesEmail = emailtext.split("@")
+if (partesEmail = emailtext.split("@"))
+
+
+
+
+
+

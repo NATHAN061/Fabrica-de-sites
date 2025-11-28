33d2e7d37baecc2fd717af6978da960c701a254c
NATHAN061 <nathanjunior061@gmail.com>
2025-11-28 17:54:10 +0000

    ultimo

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 function validaFora() {  
 cnst email = document.getElementByid('email');
 let emailtext = email.value.tria();
-let valido = false;
-if (emailtext.includes("@"));
+
+if (emailtext.includes('@')){
+valido = true
+console.log("achei @!");
 }
-else { 
+
+else {
 valido = false;
-
-
-
 }
 
-let partesEmail = emailtext.split("@")
-if (partesEmail = emailtext.split("@"))
+let partesEmail = emailtext.split("@");
+if (partesemail[0].length >= 7 && partesemail[0].length ,= 127) {
+    console.log("o tamanho está entre 7 e 127");
+    valido = false;
+}
 
-
-
-
-
-
+const nome = document.getelementbyid("nome");
+let nometext = nome.value.trim();
+if(nome.length >= 3){
+    console.log("nome atende ao tamanho minimo")
+    valido = true;
+} else{}
